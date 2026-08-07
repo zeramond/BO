92476eb015eecf3a4c87697a7204732a85d0572b
Saleh Haymoor <salehhaymoor@gmail.com>
2026-08-07 14:11:50 +0400

    Added Billiards Transition

--- src/components/SceneManager.tsx
+++ src/components/SceneManager.tsx
@@ -1,12 +1,14 @@
 import SceneTransition from "@/components/SceneTransition";
 import ScenePlaystation from "@/components/scenes/ScenePlaystation";
 import SceneCoffee from "@/components/scenes/SceneCoffee";
+import SceneBilliards from "@/components/scenes/SceneBilliards";
 
 export default function SceneManager() {
   return (
     <SceneTransition>
       <ScenePlaystation />
       <SceneCoffee />
+      <SceneBilliards />
     </SceneTransition>
   );
 }
---
+++ src/components/scenes/SceneBilliards.tsx
@@ -0,0 +1,18 @@
+import Scene from "@/components/Scene";
+
+export default function SceneBilliards() {
+  return (
+    <Scene
+      image="/images/billiards1.jpg"
+      eyebrow="BILLIARDS"
+      title={
+        <>
+          Take Your
+          <br />
+          Best Shot.
+        </>
+      }
+      description="Challenge your friends, line up the perfect shot, and keep the competition going."
+    />
+  );
+}
